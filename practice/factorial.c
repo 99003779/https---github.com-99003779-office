6ad533b12dc6982433c53d3ac65726c8b7ea2ec2
99003779 <78867425+99003779@users.noreply.github.com>
2021-02-19 23:44:06 +0530

    commiting

---
+++ practice/factorial.c
@@ -0,0 +1,28 @@
+#include "factorial.h"
+
+
+error_t find_factorial(int num, long* result)
+{
+   // long temp_result = 1;
+    if(1 == num)
+    {
+        return num;
+    }
+    else if (0 > num)
+    {
+        return NEGATIVE_NUMBER;
+    }
+    else if (0 == num)
+    {
+        return ZERO;
+    }
+    else
+    {
+        while ( 1 <= num )
+        {
+            *result *= num--;
+        }
+        return SUCCESS;
+    }
+    
+}
